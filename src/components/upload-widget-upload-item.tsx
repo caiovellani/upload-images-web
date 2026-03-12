2be8e1079bffd9fc0751a39ee6f355be7877cca4
Caio Vellani <caiovellani@outlook.com>
2026-03-12 00:01:40 -0300

    feat: image download

--- src/components/upload-widget-upload-item.tsx
+++ src/components/upload-widget-upload-item.tsx
@@ -44,8 +44,18 @@ export function UploadWidgetUploadItem({ upload, uploadId }: Props) {
           </span>
           <div className="size-1 rounded-full bg-zinc-700" />
           <span>
-            300KB
-            <span className="text-green-400 ml-1">-94%</span>
+            {formatBytes(upload.compressedSizeInBytes ?? 0)}
+            {upload.compressedSizeInBytes && (
+              <span className="text-green-400 ml-1">
+                -
+                {Math.round(
+                  ((upload.originalSizeInBytes - upload.compressedSizeInBytes) *
+                    100) /
+                    upload.originalSizeInBytes,
+                )}
+                %
+              </span>
+            )}
           </span>
           <div className="size-1 rounded-full bg-zinc-700" />
           {upload.status === "success" && <span>100%</span>}
@@ -73,9 +83,15 @@ export function UploadWidgetUploadItem({ upload, uploadId }: Props) {
       </Progress.Root>
 
       <div className="absolute top-2.5 right-2.5 flex items-center gap-1">
-        <Button disabled={upload.status !== "success"} size="icon-sm">
-          <Download className="size-4" strokeWidth={1.5} />
-          <span className="sr-only">Download compressed image</span>
+        <Button
+          asChild
+          aria-disabled={upload.status !== "success"}
+          size="icon-sm"
+        >
+          <a href={upload.remoteUrl} download>
+            <Download className="size-4" strokeWidth={1.5} />
+            <span className="sr-only">Download compressed image</span>
+          </a>
         </Button>
 
         <Button
